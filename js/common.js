d36469d74db0b282c970ae9cdc46184a262195ed
yuewei shi <2671523019@qq.com>
2018-04-13 15:16:06 +0800

    添加提示信息的功能
    (cherry picked from commit 8d8cb008477c89f1614e0031848509f3feb5b3af)

--- js/common.js
+++ js/common.js
@@ -1,6 +1,6 @@
 
-var basepath = 'http://10.1.150.143:8080';
-//var basepath = 'http://10.1.150.147:8080';
+//var basepath = 'http://10.1.150.143:8080';
+var basepath = 'http://10.1.150.147:8080';
 
 //数据加载中图片
 var img_mT = $("#Mask").height()/2-25;
